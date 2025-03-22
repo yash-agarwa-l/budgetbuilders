ad21e99ba0c3cc671cec2a564e7fe9f3da7a838b
yash <yash162534@gmail.com>
2025-03-22 20:24:56 +0530

    Remove committed node_modules and consolidate gitignore
    Delete the vendored dependency tree that was accidentally committed and
    hoist the backend gitignore to the repository root so installed packages
    stay untracked.

--- BudgetBuilders/BudgetBuilders-api-node - Copy/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const mongodbUrl="mongodb+srv://kritikag456:56Y20ra6WamBvYgi@cluster0.frs5tnc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-const connectDb=()=>{
-    return mongoose.connect(mongodbUrl);
-}
-
-module.exports={connectDb}
